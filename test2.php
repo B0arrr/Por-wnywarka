27794b4cd6268b443f1b51717c677fb041467f2b
Adrian Wałach <aw055966@student.ath.edu.pl>
2020-11-28 19:53:02 +0100

    Połączenie z bazą

---
+++ test2.php
@@ -0,0 +1,31 @@
+<?php
+    $mysql_host = "localhost";
+    $mysql_database = "id15524123_porownywarka_gier";
+    $mysql_user = "id15524123_grucha";
+    $mysql_password = "HasloDoBazyDanych12!";
+    
+    $arr_Name = null;
+    $arr_Description = null;
+    
+    try {
+      $connect = new PDO("mysql:host=$mysql_host;dbname=$mysql_database", $mysql_user, $mysql_password);
+      $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      
+        $sql = "SELECT Name, Description FROM `dbo.Games`";
+                    
+       $query = $connect->query($sql);
+       
+       $counter = 0;
+
+        foreach($query as $tmp)
+        {
+            $arr_Name[$counter] = $tmp['Name'];
+            $arr_Description[$counter] = $tmp['Description'];
+            $counter++;
+        }
+        
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
+    $conn = null;
+?>
