a02d6b63b22d9a78cf71fd4a47ba9ac1aae92cbe
Patryk Gruszczyk <pg056635@student.ath.edu.pl>
2020-12-18 23:50:13 +0100

    filtrowanie po kategoriach

--- test2.php
+++ test2.php
@@ -1,31 +1,106 @@
 <?php
-    $mysql_host = "localhost";
-    $mysql_database = "id15524123_porownywarka_gier";
-    $mysql_user = "id15524123_grucha";
-    $mysql_password = "HasloDoBazyDanych12!";
-    
-    $arr_Name = null;
-    $arr_Producent = null;
-    
-    try {
-      $connect = new PDO("mysql:host=$mysql_host;dbname=$mysql_database", $mysql_user, $mysql_password);
-      $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      
-        $sql = "SELECT Name, Producent FROM `dbo.Games`";
-                    
-       $query = $connect->query($sql);
-       
-       $counter = 0;
+$mysql_host = "localhost";
+$mysql_database = "id15524123_porownywarka_gier";
+$mysql_user = "id15524123_grucha";
+$mysql_password = "WieclawLukasz12!";
 
-        foreach($query as $tmp)
-        {
-            $arr_Name[$counter] = $tmp['Name'];
-            $arr_Description[$counter] = $tmp['Producent'];
-            $counter++;
-        }
-        
-    } catch(PDOException $e) {
-      echo "Connection failed: " . $e->getMessage();
+$arr_Name = null;
+$arr_Producent = null;
+$resultcount=null;
+
+try {
+    $connect = new PDO("mysql:host=$mysql_host;dbname=$mysql_database", $mysql_user, $mysql_password);
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    @$category= $_GET['NameOfCategory'];
+    @$shop= $_GET['NameOfShop'];
+    @$producent= $_GET['Producent'];
+  
+    $category == '' ? $b_category = 0 : $b_category = 1;
+    $shop == '' ? $b_shop = 0 : $b_shop = 1;
+    $producent == '' ? $b_producent = 0 : $b_producent = 1;
+    
+    if (!$b_category && !$b_shop && !$b_producent)
+    {   
+        $sql= "SELECT Name, Producent
+                    FROM `dbo.Games`";
     }
-    $conn = null;
+    
+    else if ($b_category && $b_shop && $b_producent)
+    {
+        $sql= "SELECT Name, Producent
+                    FROM `dbo.Games`
+                    INNER JOIN `dbo.Categories`  ON `dbo.Games`.Category = `dbo.Categories`.ID
+                    INNER JOIN `dbo.Shops` ON `dbo.Games`.Shop=`dbo.Shops`.ID
+                    WHERE NameOfCategory = '$category' AND NameOfShop = '$shop' AND Producent = '$producent'";
+    }
+    
+    else if ($b_category && $b_shop && !$b_producent)
+    {
+        $sql= "SELECT Name, Producent
+                    FROM `dbo.Games`
+                    INNER JOIN `dbo.Categories`  ON `dbo.Games`.Category = `dbo.Categories`.ID
+                    INNER JOIN `dbo.Shops` ON `dbo.Games`.Shop=`dbo.Shops`.ID
+                    WHERE NameOfCategory = '$category' AND NameOfShop = '$shop'";
+    }
+    
+    else if ($b_category && !$b_shop && $b_producent)
+    {
+        $sql= "SELECT Name, Producent
+                    FROM `dbo.Games`
+                    INNER JOIN `dbo.Categories`  ON `dbo.Games`.Category = `dbo.Categories`.ID
+                    WHERE NameOfCategory = '$category'  AND Producent = '$producent'  ";
+    }
+    
+    else if (!$b_category && $b_shop && $b_producent)
+    {
+        $sql= "SELECT Name, Producent
+                    FROM `dbo.Games`
+                    INNER JOIN `dbo.Shops` ON `dbo.Games`.Shop=`dbo.Shops`.ID
+                    WHERE  NameOfShop = '$shop' AND Producent = '$producent' ";
+    }
+    
+    else if ($b_category && !$b_shop && !$b_producent)
+    {
+        $sql= "SELECT Name, Producent
+                    FROM `dbo.Games`  
+                    INNER JOIN `dbo.Categories`  ON `dbo.Games`.Category = `dbo.Categories`.ID
+                    WHERE NameOfCategory = '$category'";
+    }
+   
+    else if (!$b_category && !$b_shop && $b_producent)
+    {
+        $sql= "SELECT Name, Producent
+                    FROM `dbo.Games`  
+                    WHERE Producent = '$producent' ";
+    }
+    
+    else if (!$b_category && $b_shop && !$b_producent)
+    {
+        $sql= "SELECT Name, Producent
+                    FROM `dbo.Games`
+                    INNER JOIN `dbo.Shops` ON `dbo.Games`.Shop=`dbo.Shops`.ID
+                    WHERE NameOfShop = '$shop' ";
+    }
+   
+    
+
+    $query = $connect->query($sql);
+
+    $counter = 0;
+
+    foreach($query as $tmp)
+    {
+        $arr_Name[$counter] = $tmp['Name'];
+        $arr_Description[$counter] = $tmp['Producent'];
+        $counter++;
+    }
+    
+    $resultcount=$counter;
+
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    
+}
+$conn = null;
 ?>
